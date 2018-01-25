3ae9cb35a7c35f832d9933b3fe04b32628152704
David Morgan <itsdavidmorgan@gmail.com>
2018-01-25 13:50:21 -1000

    Admin notice and theme description update

--- functions.php
+++ functions.php
@@ -218,6 +218,85 @@ add_action( 'wp_enqueue_scripts', 'music_lite_enqueue_scripts' );
 
 /*
 -------------------------------------------------------------------------------------------------------
+	Admin Notice
+-------------------------------------------------------------------------------------------------------
+*/
+
+/** Function music_lite_admin_notice */
+function music_lite_admin_notice() {
+	if ( ! PAnD::is_admin_notice_active( 'notice-music-lite-30' ) ) {
+		return;
+	}
+	?>
+
+	<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+		var js, fjs = d.getElementsByTagName(s)[0];
+		if (d.getElementById(id)) return;
+		js = d.createElement(s); js.id = id;
+		js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.9&appId=246727095428680";
+		fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
+
+	<script>window.twttr = (function(d, s, id) {
+		var js, fjs = d.getElementsByTagName(s)[0],
+		t = window.twttr || {};
+		if (d.getElementById(id)) return t;
+		js = d.createElement(s);
+		js.id = id;
+		js.src = "https://platform.twitter.com/widgets.js";
+		fjs.parentNode.insertBefore(js, fjs);
+
+		t._e = [];
+		t.ready = function(f) {
+			t._e.push(f);
+		};
+
+		return t;
+	}(document, "script", "twitter-wjs"));</script>
+
+	<div data-dismissible="notice-music-lite-30" class="notice updated is-dismissible">
+
+		<p><?php printf( __( 'Enter your email to receive updates and information from <a href="%1$s" target="_blank">Organic Themes</a>. Upgrade to <a href="%2$s" target="_blank">premium version</a> for more options and support.', 'music-lite' ), 'https://organicthemes.com/themes/', 'https://organicthemes.com/theme/music-theme/' ); ?></p>
+
+		<div class="follows" style="overflow: hidden; margin-bottom: 12px;">
+
+			<div id="mc_embed_signup" class="clear" style="float: left;">
+				<form action="//organicthemes.us1.list-manage.com/subscribe/post?u=7cf6b005868eab70f031dc806&amp;id=c3cce2fac0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+					<div id="mc_embed_signup_scroll">
+						<div id="mce-responses" class="clear">
+							<div class="response" id="mce-error-response" style="display:none"></div>
+							<div class="response" id="mce-success-response" style="display:none"></div>
+						</div>
+						<div class="mc-field-group" style="float: left;">
+							<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Email Address">
+						</div>
+						<div style="float: left; margin-left: 6px;"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+						<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_7cf6b005868eab70f031dc806_c3cce2fac0" tabindex="-1" value=""></div>
+					</div>
+				</form>
+			</div>
+
+			<div class="social-links" style="float: left; margin-left: 24px; margin-top: 4px;">
+				<div class="fb-like" style="float: left;" data-href="https://www.facebook.com/OrganicThemes/" data-layout="button_count" data-action="like" data-size="small" data-show-faces="false" data-share="false"></div>
+				<div class="twitter-follow" style="float: left; margin-left: 6px;"><a class="twitter-follow-button" href="https://twitter.com/OrganicThemes" data-show-count="false">Follow @OrganicThemes</a></div>
+			</div>
+
+		</div>
+
+	</div>
+
+	<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+	<!--End mc_embed_signup-->
+	<?php
+}
+add_action( 'admin_init', array( 'PAnD', 'init' ) );
+add_action( 'admin_notices', 'music_lite_admin_notice' );
+
+require( get_template_directory() . '/includes/persist-admin-notices-dismissal/persist-admin-notices-dismissal.php' );
+
+/*
+-------------------------------------------------------------------------------------------------------
 	Category ID to Name
 -------------------------------------------------------------------------------------------------------
 */
