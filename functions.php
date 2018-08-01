0dd319d447e42f75f909f98adc2cccb165230c00
David Morgan <itsdavidmorgan@gmail.com>
2018-08-01 17:57:45 -0600

    Fixed issue of custom admin submenu link overwriting other links

--- functions.php
+++ functions.php
@@ -237,14 +237,14 @@ add_action( 'admin_enqueue_scripts', 'music_lite_enqueue_admin_scripts' );
 function music_lite_support_link() {
 	global $submenu;
 	$support_link = esc_url( 'https://organicthemes.com/support/' );
-	$submenu['themes.php'][6] = array( __( 'Theme Support', 'music-lite' ), 'manage_options', $support_link );
+	$submenu['themes.php'][] = array( __( 'Theme Support', 'music-lite' ), 'manage_options', $support_link );
 }
 add_action( 'admin_menu', 'music_lite_support_link' );
 
 function music_lite_upgrade_link() {
 	global $submenu;
 	$upgrade_link = esc_url( 'https://organicthemes.com/theme/music-theme/' );
-	$submenu['themes.php'][7] = array( __( 'Theme Upgrade', 'music-lite' ), 'manage_options', $upgrade_link );
+	$submenu['themes.php'][] = array( __( 'Theme Upgrade', 'music-lite' ), 'manage_options', $upgrade_link );
 }
 add_action( 'admin_menu', 'music_lite_upgrade_link' );
 
